be781454dbccc5b5876bdf67218ac38d97120cf6
Niki Bizjak <drobilc@gmail.com>
2022-12-19 08:36:11 +0100

    Merge `int` and `float` to `num`

--- lib/visitors/datatype/datatype_merger.dart
+++ lib/visitors/datatype/datatype_merger.dart
@@ -3,12 +3,18 @@ import 'package:inferno/datatypes/datatypes.dart';
 import 'package:inferno/visitors/datatype/datatype_matcher.dart';
 
 class DataTypeMerger {
-
   static DataType mergeFirst(DataType first, DataType second) {
     return first;
   }
 
   static DataType mergeNonMatchingToDynamic(DataType first, DataType second) {
+    // If both data types are numeric, but don't match exactly (eg. one is a
+    // float and the other is a int), then the field should have a `num` data
+    // type.
+    if (first != second && first is NumberType && second is NumberType) {
+      return NumberType(nullable: first.nullable || second.nullable);
+    }
+
     // If the types don't match, perform no inference any simply return the
     // dynamic type.
     if (DataTypeMatcher.match(first, second) != Match.fullMatch) {
@@ -25,6 +31,11 @@ class DataTypeMerger {
       return first;
     } else if (first is NullType && second is ObjectType) {
       return second;
+    } else if (first != second && first is NumberType && second is NumberType) {
+      // If both data types are numeric, but don't match exactly (eg. one is a
+      // float and the other is a int), then the field should have a `num` data
+      // type.
+      return NumberType(nullable: first.nullable || second.nullable);
     } else if (first is ObjectType && second is ObjectType) {
       final firstFieldNames = first.fieldTypes.keys.toSet();
       final secondFieldNames = second.fieldTypes.keys.toSet();
@@ -46,7 +57,7 @@ class DataTypeMerger {
         ...onlyInFirst.map((e) => MapEntry(e.key, e.value.makeNullable(true))),
         ...onlyInSecond.map((e) => MapEntry(e.key, e.value.makeNullable(true))),
       ];
-      
+
       return ObjectType(Map.fromEntries(allFields));
     } else if (DataTypeMatcher.match(first, second) == Match.fullMatch) {
       return first;
